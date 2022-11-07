eb0755c7283571ea9d56f42e7626557eb14d1510
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2022-11-07 14:32:10 +0700

    test case 04 author login new test

--- cypress/e2e/04-author_login.cy.js
+++ cypress/e2e/04-author_login.cy.js
@@ -8,11 +8,16 @@ describe('Author Wants to Login with valid credential', ()=>{
     it('author redirect to login page', ()=>{
         cy.contains('Author').click({force: true})
 
+        cy.wait(3000)
         cy.get('[data-testid="email"]').type('testing@gmail.com', {force: true})
+
+        cy.wait(3000)
         cy.get('[data-testid="password"]').type('testing', {force: true})
         
+        cy.wait(3000)
         cy.get('[data-testid="submit-login-btn-author"]').click({force: true})
 
+        cy.wait(5000)
         cy.url().should('eq', 'https://ecerpen-dev.herokuapp.com/home')
     })
 
